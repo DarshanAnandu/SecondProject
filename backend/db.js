f66d5f002f5a879d9465e885d0832dd8a5220c60
DarshanAnandu <darshansiva32@gmail.com>
2023-03-25 10:52:51 +0530

    Serious Commit

--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Bigil:udnanADarshan@leo.alqcopa.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://Bigil:12345abcde6789@leo.alqcopa.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
